a3d8114be04ea00ade9eea48a10a450f6318c0ee
mateomotriz <49557156+mateomotriz@users.noreply.github.com>
2019-05-07 17:56:29 +0200

    Update OuterFragment.java

--- app/src/main/java/de/marei/fragmentsandnestedfragments/fragments/OuterFragment.java
+++ app/src/main/java/de/marei/fragmentsandnestedfragments/fragments/OuterFragment.java
@@ -19,7 +19,10 @@ public class OuterFragment extends Fragment implements ButtonCallback {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, 
+                             @Nullable Bundle savedInstanceState) {
+        
         return inflater.inflate(R.layout.fragment_outer, container, false);
     }
 
